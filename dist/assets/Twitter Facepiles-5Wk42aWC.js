515e85189ccfade591d7de17cc4a8d2eb9c5d0a5
Christopher Dellosa <christopher.j.dellosa@gmail.com>
2026-02-11 17:23:01 -0800

    Add pre-built dist folder for deployment

---
+++ dist/assets/Twitter Facepiles-5Wk42aWC.js
@@ -0,0 +1,16 @@
+const e=`## Twitter Creative Systems: Facepiles Research
+
+In this UX research experiment prototype, we're testing the inclusion of face piles (user profile avatars in a circle) that appear in real-time when users engage with a tweet. We have three different parameters with two options each we are testing:
+
+- Parameter 1: Facepiles On/Off
+    - A: Facepiles On
+    - B: Facepiles Off
+- Parameter 2: Facepile Direction
+    - Y: Left to Right
+    - Z: Right to Left
+- Parameter 3: Facepile Animation
+    - O: Animation On
+    - P: Animation Off
+
+The password to access the demo is "larry".
+`;export{e as default};
---
+++ dist/assets/Twitter Message Threads-C0SLcXjy.js
@@ -0,0 +1,15 @@
+const e=`## Creative Systems: Make Twitter Easy (Threads) Research
+
+In this UX research experiment, we're testing different experiences to help our users easily create threaded Tweets. We have two different parameters with three and two options each we are testing:
+
+- Parameter 1: Character count to trigger actions
+    - A: Fast (5 characters)
+    - B: Normal (20 characters)
+    - C: Slow (50 characters)
+
+- Parameter 2: Thread trigger options
+    - Y: Tooltip (Tooltip appears in lower left hand corner to suggest making a tweet into a threaded tweet)
+    - Z: Accelerator (Thread is automatically created after character count is reached)
+
+The password to access the demo is "larry".
+`;export{e as default};
